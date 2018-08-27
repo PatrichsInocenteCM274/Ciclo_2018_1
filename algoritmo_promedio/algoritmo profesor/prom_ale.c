7ce291ea63ed64f9de3abf8155db17437a4ff58a
Patrichs Inocente <inocentepatrichs@gmail.com>
2018-08-27 01:58:07 -0500

    actualizando

---
+++ algoritmo_promedio/algoritmo profesor/prom_ale.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+void main(){
+  int n,i,j,k;
+  int *X;
+  float *A,a,tiempo;
+  for(k=1;k<10000;k=k+100){
+  n=k;
+  X=(int*)malloc(n*sizeof(int));
+  A=(float*)malloc(n*sizeof(float));
+  
+  srand (time(NULL));
+  for(i=0;i<n;i++) X[i]=rand()%10001;
+  
+  tiempo=clock();
+  for(i=0;i<n;i++){
+     a=0;
+     for(j=0;j<=i;j++) a=a+X[j];
+     A[i]=a/(i+1);	
+  }
+  tiempo=clock()-tiempo;
+  printf("%d %f\n",n,tiempo/1000);
+  }
+  /*Muestra de los valores obtenidos
+  for(i=0;i<n;i++) printf("%d ",X[i]);
+  printf("\n");
+  for(i=0;i<n;i++) printf("%.3f ",A[i]);*/
+}
